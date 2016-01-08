6982b8b077ec2bef10bd364fed7f8eee8ff95ba4
Marziyeh Ahmed <marziyehahmed@Marziyehs-MacBook-Pro.local>
2016-01-08 13:05:05 +0000

    full english lyrics displaying - search by artist and song title #3

--- lyrics.js
+++ lyrics.js
@@ -2,37 +2,65 @@ var https = require('https');
 var querystring = require('querystring');
 var env = require('env2')('./config.env');
 var http = require('http');
+var parser = require('xml2json');
 
-var api = "http://api.musixmatch.com/ws/1.1/";
+function trackHandler(req,res){
+  res.writeHead(200, {'Content-Type' : 'text/html'});
+  getSongDetails(req,res,returnLyrics);
+};
 
-function lyricsHandler(handlerReq,handlerRes){
+
+function getSongDetails(req,res, callback){
+  var body = '';
+  req.on('data', function (dataChunk) {
+      body += dataChunk;
+  });
+  req.on('end', function () {
+    var entries = body.split('&');
+    var deets = entries.map(splitByEquals);
+    var artist=deets[0][1];
+    var track=deets[1][1];
+    var language=deets[2][1];
+    console.log(artist,track,language);
+
+    callback(artist,track,language,res);
+  });
+}
+
+function splitByEquals(arg){
+  return arg.split("=");
+}
+
+function returnLyrics(artist,track,language,handlerRes){
   var body ='';
   var lyrics = '';
 
-  handlerRes.writeHead(200, {'Content-Type' : 'text/html'});
+  var api = "http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect?";
 
-  var reqString = api + 'matcher.lyrics.get?'+
-      querystring.stringify({q_track:"Gimme gimme gimme", q_artist:"Abba", apikey:process.env.apiKey});
+  var ApiReqString = api +
+      querystring.stringify({ artist:artist,song:track});
+      console.log(ApiReqString);
+// apikey:process.env.apiKey
 
-  console.log(reqString);
+console.log(ApiReqString);
 
-  http.get(reqString, function(res){
-
+  http.get(ApiReqString, function(res){
     res.on('data', function(chunk) {
-      body+=chunk;
-    });
+        body+=chunk;
+      });
 
     res.on('end', function(){
-     var data = JSON.parse(body)
-     lyrics = data.message.body.lyrics.lyrics_body;
-     console.log("im' lyrics", lyrics);
-     handlerRes.end("<b>"+lyrics+"</b>");
+      var xmlObj=body;
+      jsonObj = parser.toJson(xmlObj);
+      jsonObjParsed=JSON.parse(jsonObj);
+      lyrics=jsonObjParsed['GetLyricResult']['Lyric'];
+      handlerRes.end("<p>"+lyrics+"</p>");
+        });
     });
-  });
 };
 
 
 
 module.exports = {
-  lyricsHandler: lyricsHandler
+  trackHandler: trackHandler
 };
